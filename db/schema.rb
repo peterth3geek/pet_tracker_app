bd7e41c4277009bd79807a345e60c212bae8ad23
github username <github email address>
2018-07-30 17:26:18 -0400

    new time column on appointments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_30_203832) do
+ActiveRecord::Schema.define(version: 2018_07_30_212534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2018_07_30_203832) do
     t.integer "owner_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "time"
   end
 
   create_table "conditions", force: :cascade do |t|
