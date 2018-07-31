49e741ac64077db83b24eeaebcbdee7482636590
github username <github email address>
2018-07-31 10:22:00 -0400

    mostly view improvements

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -12,4 +12,15 @@ class ConditionsController < ApplicationController
     @condition = Condition.new
   end
 
+  def create
+    binding.pry
+    @condition = Condition.create(condition_params)
+    redirect_to condition_path(@condition)
+  end
+
+  private
+  def condition_params
+    params.require(:condition).permit(:title, :notes, :pet_id)
+  end
+
 end
